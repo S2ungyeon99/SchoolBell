c9a1a1b4edb6307fdb5972b453df49318ef24c5a
S2ungyeon <haneon49@gmail.com>
2025-06-12 03:02:15 +0900

    Firebase 설정 추가 및 환경 구성

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+// firebase/firebaseConfig.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyD6bifhu9yAL9jbQdFgGnV7pHPdqGBFTuk",
+  authDomain: "schoolbell-aaa7b.firebaseapp.com",
+  projectId: "schoolbell-aaa7b",
+  storageBucket: "schoolbell-aaa7b.appspot.com",
+  messagingSenderId: "110037957022",
+  appId: "1:110037957022:android:afe83bd4f7487478a2be8f"
+};
